c5dbca67228a46f15daccfba727e3eb72cff805c
Martin Miklós <miklos.martin@gmail.com>
2014-05-07 21:29:14 +0200

    removed dead code

--- src/Onend/PayPal/Common/Model/AbstractModel.php
+++ src/Onend/PayPal/Common/Model/AbstractModel.php
@@ -4,38 +4,6 @@ namespace Onend\PayPal\Common\Model;
 
 abstract class AbstractModel
 {
-    public function toJSON()
-    {
-        return json_encode($this->toArray());
-    }
-
-    public function toArray()
-    {
-        return $this->convertToArray(get_object_vars($this));
-    }
-
-    protected function convertToArray($param)
-    {
-        $ret = [];
-        foreach ($param as $k => $v) {
-
-            if ($v instanceof AbstractModel) {
-                $ret[$k] = $v->toArray();
-            } else {
-                if (is_array($v)) {
-                    $ret[$k] = $this->convertToArray($v);
-                } else {
-                    if ($v !== null) {
-                        $ret[$k] = $v;
-                    }
-                }
-            }
-        }
-
-        return $ret;
-    }
-
-
     /**
      * @return string
      */
